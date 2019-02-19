b05468cbe3ed1cc92ed275ebb129163d336eae0f
ChileWang0228 <912427166@qq.com>
2019-02-19 17:11:59 +0800

    龙华招标爬虫

--- scrapy_spider/szzfcg_longhua_project/szzfcg_longhua_project/spiders/szzfcg_longhua_project_spider.py
+++ scrapy_spider/szzfcg_longhua_project/szzfcg_longhua_project/spiders/szzfcg_longhua_project_spider.py
@@ -68,7 +68,11 @@ class SzzcfgLonghuaProjectSpider(scrapy.Spider):
         bidding_open_time = html_content.xpath("./ol")[0].xpath('./li').xpath('string(.)').extract()[-2]  # 开标时间
         bidding_end_time = html_content.xpath("./ol")[0].xpath('./li').xpath('string(.)').extract()[-1]  # 投标截止时间
         bidding_agency = ""  # 代理机构
-        item = SzzfcgLonghuaProjectItem(publish_time=publish_time, ip=ip,
+
+        # 重新构造item
+        item = response.meta['item']
+        website_name = item['website_name']
+        item = SzzfcgLonghuaProjectItem(website_name=website_name, publish_time=publish_time, ip=ip,
                                           bidding_name=bidding_name, bidding_url=bidding_url,
                                           bidding_content=bidding_content, bidding_tenderee=bidding_tenderee,
                                           bidding_block=bidding_block, bidding_open_time=bidding_open_time,
